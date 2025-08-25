f2f67cf13c5901d026d27b7d3a333fef4907fa52
KirillSilesia <kiralihtenberg2@gmail.com>
2025-08-25 10:21:36 +0200

    --testing--

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -16,6 +16,10 @@ class RegistrationPage:
         self.gdpr_checkbox = (By.ID, "gdprAccepted")
         self.registration_submit_button = (By.XPATH, "//*[contains(text(), 'Register')]")
         self.back_button = (By.XPATH, "//*[contains(text(), 'Back')]")
+        self.success_message = (By.XPATH, "//*[contains(text(), 'we just sent you a confirmation link')]")
+        self.error_message = (By.CLASS_NAME, "register-error-text")
+        self.register_error = (By.CLASS_NAME, "register-error-message")
+        self.checkbox_error_message = (By.CLASS_NAME, "school-reg-error")
 
     def fill_valid_email(self):
         email_element = WebDriverWait(self.driver, 10).until(
@@ -24,16 +28,34 @@ class RegistrationPage:
         email_element.send_keys(random_email())
 
     def fill_special_email(self):
-        self.driver.find_element(*self.email_input).sendkeys(random_email_special())
+        email_element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.email_input)
+        )
+        self.driver.find_element(*self.email_input).send_keys(random_email_special())
 
     def fill_no_domen_email(self):
-        self.driver.find_element(*self.email_input).sendkeys(random_email_no_domen())
+        email_element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.email_input)
+        )
+        self.driver.find_element(*self.email_input).send_keys(random_email_no_domen())
 
     def fill_double_at_email(self):
-        self.driver.find_element(*self.email_input).sendkeys(random_email_double_at())
+        email_element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.email_input)
+        )
+        self.driver.find_element(*self.email_input).send_keys(random_email_double_at())
 
     def fill_without_at_email(self):
-        self.driver.find_element(*self.email_input).sendkeys(random_email_without_at())
+        email_element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.email_input)
+        )
+        self.driver.find_element(*self.email_input).send_keys(random_email_without_at())
+
+    def fill_existing_email(self):
+        email_element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.email_input)
+        )
+        self.driver.find_element(*self.email_input).send_keys("johnsenior12345678@gmail.com")
 
     def fill_username(self):
         self.driver.find_element(*self.username_input).send_keys("John doe")
@@ -41,11 +63,29 @@ class RegistrationPage:
     def fill_valid_password(self):
         self.driver.find_element(*self.password_input).send_keys("12345678")
 
+    def fill_too_short_password(self):
+        self.driver.find_element(*self.password_input).send_keys("1234")
+
+    def fill_repeat_too_short_password(self):
+        self.driver.find_element(*self.repeat_password_input).send_keys("1234")
+
     def fill_repeat_valid_password(self):
         self.driver.find_element(*self.repeat_password_input).send_keys("12345678")
 
+    def fill_repeat_not_same_password(self):
+        self.driver.find_element(*self.repeat_password_input).send_keys("123456789")
+
+    def fill_too_long_password(self):
+        self.driver.find_element(*self.password_input).send_keys("12345678910111213141516171819202122232425262728293031323334353637383940123456789012345678901234567890")
+
+    def fill_repeat_too_long_password(self):
+        self.driver.find_element(*self.repeat_password_input).send_keys("12345678910111213141516171819202122232425262728293031323334353637383940123456789012345678901234567890")
+
     def fill_valid_grade(self):
         self.driver.find_element(*self.grade_input).send_keys("8")
+
+    def fill_invalid_grade(self):
+        self.driver.find_element(*self.grade_input).send_keys("9")
 
     def fill_terms_checkbox(self):
         self.driver.find_element(*self.terms_checkbox).click()
@@ -54,4 +94,31 @@ class RegistrationPage:
         self.driver.find_element(*self.gdpr_checkbox).click()
 
     def submit_registration(self):
+        register_element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.registration_submit_button)
+        )
         self.driver.find_element(*self.registration_submit_button).click()
+
+    def get_success_message(self):
+        message_element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.success_message)
+        )
+        return self.driver.find_element(*self.success_message).text
+
+    def get_error_message(self):
+        error_element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.error_message)
+        )
+        return self.driver.find_element(*self.error_message).text
+
+    def checkbox_error_message(self):
+        checkbox_element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.checkbox_error_message)
+        )
+        return self.driver.find_element(*self.checkbox_error_message).text
+
+    def register_error_message(self):
+        register_element = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located(self.register_error)
+        )
+        return self.driver.find_element(*self.register_error).text
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,4 +1,5 @@
 from generators.email_generators import email_random
+from pages import registration_page
 from utils.assertions import assert_url_contains
 from pages.main_page import MainPage
 from pages.registration_page import RegistrationPage
@@ -23,3 +24,171 @@ def test_registration_valid_data(driver):
     registration_page.fill_terms_checkbox()
     registration_page.fill_gdpr_checkbox()
     registration_page.submit_registration()
+    assert registration_page.get_success_message() is not None
+
+def test_registration_special_email(driver):
+    main_page = MainPage(driver)
+    registration_page = RegistrationPage(driver)
+
+    main_page.go_to_registration()
+
+    registration_page.fill_special_email()
+    registration_page.fill_username()
+    registration_page.fill_valid_password()
+    registration_page.fill_repeat_valid_password()
+    registration_page.fill_valid_grade()
+    registration_page.fill_terms_checkbox()
+    registration_page.fill_gdpr_checkbox()
+    registration_page.submit_registration()
+    assert registration_page.get_success_message() is not None
+
+def test_registration_no_domen_email(driver):
+    main_page = MainPage(driver)
+    registration_page = RegistrationPage(driver)
+
+    main_page.go_to_registration()
+
+    registration_page.fill_no_domen_email()
+    registration_page.fill_username()
+    registration_page.fill_valid_password()
+    registration_page.fill_repeat_valid_password()
+    registration_page.fill_valid_grade()
+    registration_page.fill_terms_checkbox()
+    registration_page.fill_gdpr_checkbox()
+    registration_page.submit_registration()
+    assert registration_page.get_error_message()
+
+def test_registration_double_at_email(driver):
+    main_page = MainPage(driver)
+    registration_page = RegistrationPage(driver)
+
+    main_page.go_to_registration()
+
+    registration_page.fill_double_at_email()
+    registration_page.fill_username()
+    registration_page.fill_valid_password()
+    registration_page.fill_repeat_valid_password()
+    registration_page.fill_valid_grade()
+    registration_page.fill_terms_checkbox()
+    registration_page.fill_gdpr_checkbox()
+    registration_page.submit_registration()
+    assert registration_page.get_error_message()
+
+def test_registration_without_at_email(driver):
+    main_page = MainPage(driver)
+    registration_page = RegistrationPage(driver)
+
+    main_page.go_to_registration()
+
+    registration_page.fill_without_at_email()
+    registration_page.fill_username()
+    registration_page.fill_valid_password()
+    registration_page.fill_repeat_valid_password()
+    registration_page.fill_valid_grade()
+    registration_page.fill_terms_checkbox()
+    registration_page.fill_gdpr_checkbox()
+    registration_page.submit_registration()
+    assert registration_page.get_error_message()
+
+def test_registration_with_too_short_password(driver):
+    main_page = MainPage(driver)
+    registration_page = RegistrationPage(driver)
+
+    main_page.go_to_registration()
+
+    registration_page.fill_valid_email()
+    registration_page.fill_username()
+    registration_page.fill_too_short_password()
+    registration_page.fill_repeat_too_short_password()
+    registration_page.fill_valid_grade()
+    registration_page.fill_terms_checkbox()
+    registration_page.fill_gdpr_checkbox()
+    registration_page.submit_registration()
+    assert registration_page.get_error_message()
+
+def test_registration_with_not_same_password(driver):
+    main_page = MainPage(driver)
+    registration_page = RegistrationPage(driver)
+
+    main_page.go_to_registration()
+
+    registration_page.fill_valid_email()
+    registration_page.fill_username()
+    registration_page.fill_valid_password()
+    registration_page.fill_repeat_not_same_password()
+    registration_page.fill_valid_grade()
+    registration_page.fill_terms_checkbox()
+    registration_page.fill_gdpr_checkbox()
+    registration_page.submit_registration()
+    assert registration_page.get_error_message()
+
+def test_with_invalid_grade(driver):
+    main_page = MainPage(driver)
+    registration_page = RegistrationPage(driver)
+
+    main_page.go_to_registration()
+
+    registration_page.fill_valid_email()
+    registration_page.fill_username()
+    registration_page.fill_valid_password()
+    registration_page.fill_repeat_valid_password()
+    registration_page.fill_invalid_grade()
+    registration_page.fill_terms_checkbox()
+    registration_page.fill_gdpr_checkbox()
+    registration_page.submit_registration()
+    assert registration_page.get_error_message()
+
+def test_registration_without_checkboxes(driver):
+    main_page = MainPage(driver)
+    registration_page = RegistrationPage(driver)
+
+    main_page.go_to_registration()
+
+    registration_page.fill_valid_email()
+    registration_page.fill_username()
+    registration_page.fill_valid_password()
+    registration_page.fill_repeat_valid_password()
+    registration_page.fill_valid_grade()
+    registration_page.submit_registration()
+    assert registration_page.checkbox_error_message is not None
+
+def test_registration_with_empty_fields(driver):
+    main_page = MainPage(driver)
+    registration_page = RegistrationPage(driver)
+
+    main_page.go_to_registration()
+
+    registration_page.submit_registration()
+    assert registration_page.get_error_message()
+
+def test_registration_with_too_long_password(driver):
+    main_page = MainPage(driver)
+    registration_page = RegistrationPage(driver)
+
+    main_page.go_to_registration()
+
+    registration_page.fill_valid_email()
+    registration_page.fill_username()
+    registration_page.fill_too_long_password()
+    registration_page.fill_repeat_too_long_password()
+    registration_page.fill_valid_grade()
+    registration_page.fill_terms_checkbox()
+    registration_page.fill_gdpr_checkbox()
+    registration_page.submit_registration()
+    assert registration_page.get_error_message()
+
+def test_registration_with_existing_email(driver):
+    main_page = MainPage(driver)
+    registration_page = RegistrationPage(driver)
+
+    main_page.go_to_registration()
+
+    registration_page.fill_existing_email()
+    registration_page.fill_username()
+    registration_page.fill_valid_password()
+    registration_page.fill_repeat_valid_password()
+    registration_page.fill_valid_grade()
+    registration_page.fill_terms_checkbox()
+    registration_page.fill_gdpr_checkbox()
+    registration_page.submit_registration()
+    assert registration_page.register_error_message is not None
